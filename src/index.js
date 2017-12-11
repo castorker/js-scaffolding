28b7d2dc3086f9f355ca6dfa682b94a4baf27e0c
Eduardo <eduardo.cxcorreio@gmail.com>
2017-12-11 12:04:18 +0100

    Bundling: Webpack. Configured Webpack and Sourcemaps for debugging.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+import numeral from 'numeral';
+
+const bitcoinValue = numeral(14186.07).format('€0,0.00');
+// debugger;
+console.log(`1 Bitcoin equals ${bitcoinValue} Euro at ${new Date(Date.now()).toLocaleString()}`);
